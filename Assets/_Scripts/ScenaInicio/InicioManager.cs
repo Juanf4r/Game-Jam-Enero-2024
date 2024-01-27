9a2f40051eebfec7eea2e4a9f995cdc5ffecea45
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-01-26 22:30:12 -0600

    Inicio manager

--- Assets/_Scripts/ScenaInicio/InicioManager.cs
+++ Assets/_Scripts/ScenaInicio/InicioManager.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InicioManager : MonoBehaviour
 {
+    [SerializeField] Slider sonidoSlider;
+    [SerializeField] Slider musicaSlider;
     public void Exit()
     {
         Application.Quit();
@@ -13,6 +14,51 @@ public class InicioManager : MonoBehaviour
     public void cambioDeEscena(int Escena)
     {
         SceneManager.LoadScene(Escena);
+    }
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("SoundVolume"))
+        {
+            PlayerPrefs.SetFloat("SoundVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        if (!PlayerPrefs.HasKey("MusicVolume"))
+        {
+            PlayerPrefs.SetFloat("MusicVolume", 1);
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolumeS()
+    {
+        AudioListener.volume = sonidoSlider.value;
+        SaveS();
+    }
+    public void ChangeVolumeM()
+    {
+        AudioListener.volume = musicaSlider.value;
+        SaveM();
+    }
+    private void Load()
+    {
+        sonidoSlider.value = PlayerPrefs.GetFloat("SoundVolume");
+        musicaSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+    private void SaveS()
+    {
+        PlayerPrefs.SetFloat("SoundVolume", sonidoSlider.value);
+    }
+    private void SaveM()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicaSlider.value);
     }
 
 }
