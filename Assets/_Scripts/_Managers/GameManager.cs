35267c62f6720724baecdd24526aae7d1f12bea6
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2024-01-26 22:41:50 -0600

    Input System & placeHolders

---
+++ Assets/_Scripts/_Managers/GameManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] private GameObject mainCanvas;
+    [SerializeField] private GameObject exitPanel;
+
+    private PlayerInput _playerInput;
+    private ControllerActions _controllerActions;
+
+    private bool _stopGame;
+
+
+    private void Awake()
+    {
+        //SingleTon
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        //Input System
+        _playerInput = GetComponent<PlayerInput>();
+        _controllerActions = new ControllerActions();
+        _controllerActions.GameController.Enable();
+        _controllerActions.GameController.ExitGame.performed += EscapeButton;
+
+
+        //Active Canvas
+        mainCanvas.SetActive(true);
+    }
+
+    private void Start()
+    {
+        _stopGame = false;
+    }
+
+    #region Input System
+
+    private void OnEnable()
+    {
+        _controllerActions.GameController.Enable();
+        _controllerActions.GameController.ExitGame.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controllerActions.GameController.Disable();
+        _controllerActions.GameController.ExitGame.Disable();
+    }
+
+    public void EscapeButton(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if(_stopGame == false)
+            {
+                _stopGame = true;
+                exitPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else if(_stopGame == true)
+            {
+                _stopGame = false;
+                exitPanel.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Buttons
+
+    public void EscapeButtonResume()
+    {
+        _stopGame = false;
+        exitPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void EscapeButtonSucess()
+    {
+        //Change Scene
+    }
+
+    #endregion
+
+}
