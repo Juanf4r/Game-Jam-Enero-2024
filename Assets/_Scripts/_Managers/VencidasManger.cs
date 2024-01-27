9216c2bc630c557d53917cc07febdd69f2cc62ba
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-01-27 14:51:30 -0600

    panel de perder y ganar

--- Assets/_Scripts/_Managers/VencidasManger.cs
+++ Assets/_Scripts/_Managers/VencidasManger.cs
@@ -10,7 +10,7 @@ public class VencidasManager : MonoBehaviour
     public GameObject PanelGanador;
     public GameObject PanelPerdedor;
     private float _maxScore = 150f;
-    private float _scoreDecreaseRate = 40f;
+    private float _scoreDecreaseRate = 25f;
     private float _clickValue = 6f;
 
     private float currentScore;
@@ -58,6 +58,6 @@ public class VencidasManager : MonoBehaviour
     public void ResetGame()
     {
         PanelGanador.SetActive(false);
-        PanelPerdedor.SetActive(true);
+        PanelPerdedor.SetActive(false);
     }
 }
