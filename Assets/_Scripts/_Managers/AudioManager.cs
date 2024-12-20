6fe286ef1f8f6a95c283b4c65209aeda7677b38c
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-12-19 19:02:41 -0600

    el sonido

--- Assets/_Scripts/_Managers/AudioManager.cs
+++ Assets/_Scripts/_Managers/AudioManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public float musicVolume;
+    public float soundVolume;
     [SerializeField] private AudioSource _musica;
+    [SerializeField] private AudioSource _sound;
     public void SaveMusica()
     {
         musicVolume = InicioManager.Instance._musicaSlider.value;
         _musica.volume = musicVolume;
+        soundVolume = InicioManager.Instance._sonidoSlider.value;
+        _sound.volume = soundVolume;
     }
     private void Update()
     {
